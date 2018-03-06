d94e5568befe43d8fdb92f0dd5677cf6b26321dc
dividedbyxiro <dividedbyxiro@gmail.com>
2018-03-05 22:45:03 -0600

    set Gamecam and viewPort
    currently using FitViewport with resolution 800x480

--- core/src/com/xiro/game/Screens/PlayScreen.java
+++ core/src/com/xiro/game/Screens/PlayScreen.java
@@ -8,7 +8,12 @@ package com.xiro.game.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.xiro.game.MarioBros;
 
 /**
@@ -19,11 +24,16 @@ public class PlayScreen implements Screen
 {
 	private MarioBros game;
 	private Texture texture;
+	private OrthographicCamera gameCam;
+	private Viewport gamePort;
 	
 	public PlayScreen(MarioBros game)
 	{
 		this.game = game;
 		texture = new Texture("badlogic.jpg");
+		gameCam = new OrthographicCamera();
+		gamePort = new FitViewport(800, 480, gameCam);
+		
 	}
 
 	@Override
@@ -37,6 +47,7 @@ public class PlayScreen implements Screen
 	{
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		game.batch.setProjectionMatrix(gameCam.combined);
 		game.batch.begin();
 		game.batch.draw(texture, 0, 0);
 		game.batch.end();
@@ -45,31 +56,32 @@ public class PlayScreen implements Screen
 	@Override
 	public void resize(int width, int height)
 	{
+		gamePort.update(width, height);
 		//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 	}
 
 	@Override
 	public void pause()
 	{
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 	}
 
 	@Override
 	public void resume()
 	{
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 	}
 
 	@Override
 	public void hide()
 	{
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 	}
 
 	@Override
 	public void dispose()
 	{
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 	}
 	
 }
