558690cead5ceed1720ce6d6da8a6e22bab3a39a
dividedbyxiro <dividedbyxiro@gmail.com>
2018-03-12 16:00:16 -0500

    Added object layers from tmx to PlayScreen
    Added the object layers from Tiled tmx level 1-1 to the World in PlayScreen. Created Box@DDebugRenderer to display where the object bounds are.

--- core/src/com/xiro/game/Screens/PlayScreen.java
+++ core/src/com/xiro/game/Screens/PlayScreen.java
@@ -10,9 +10,19 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -36,6 +46,10 @@ public class PlayScreen implements Screen
 	private TiledMap map;
 	private OrthogonalTiledMapRenderer renderer;
 	
+	private World world;
+	private Box2DDebugRenderer b2dr;
+	
+	
 	public PlayScreen(MarioBros game)
 	{
 		this.game = game;
@@ -49,6 +63,60 @@ public class PlayScreen implements Screen
 		renderer = new OrthogonalTiledMapRenderer(map);
 		
 		gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+		
+		world = new World(new Vector2(0, 0), true);
+		b2dr = new Box2DDebugRenderer();
+		
+		BodyDef bdef = new BodyDef();
+		PolygonShape shape = new PolygonShape();
+		FixtureDef fdef = new FixtureDef();
+		Body body;
+		
+		for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class))
+		{
+			Rectangle rect = ((RectangleMapObject)object).getRectangle();
+			bdef.type = BodyDef.BodyType.StaticBody;
+			bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+			
+			body = world.createBody(bdef);
+			shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+			fdef.shape = shape;
+			body.createFixture(fdef);
+		}
+		for(MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class))
+		{
+			Rectangle rect = ((RectangleMapObject)object).getRectangle();
+			bdef.type = BodyDef.BodyType.StaticBody;
+			bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+			
+			body = world.createBody(bdef);
+			shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+			fdef.shape = shape;
+			body.createFixture(fdef);
+		}
+		for(MapObject object : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class))
+		{
+			Rectangle rect = ((RectangleMapObject)object).getRectangle();
+			bdef.type = BodyDef.BodyType.StaticBody;
+			bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+			
+			body = world.createBody(bdef);
+			shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+			fdef.shape = shape;
+			body.createFixture(fdef);
+		}
+		for(MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class))
+		{
+			Rectangle rect = ((RectangleMapObject)object).getRectangle();
+			bdef.type = BodyDef.BodyType.StaticBody;
+			bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+			
+			body = world.createBody(bdef);
+			shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+			fdef.shape = shape;
+			body.createFixture(fdef);
+		}
+		
 		
 	}
 	
@@ -86,6 +154,8 @@ public class PlayScreen implements Screen
 		
 		game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
 		hud.stage.draw();
+		
+		b2dr.render(world, gameCam.combined);
 	}
 
 	@Override
